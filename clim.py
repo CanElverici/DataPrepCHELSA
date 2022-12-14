5551a462f536d7b1fcea67b1e2766f4c2f03a5b0
Can Elverici <elverici.can@gmail.com>
2022-12-14 17:33:40 -0600

    V_0.0.0.2
    V_0.0.0.2 works without any issues with "1981-2010", "2011-2040", "2041-2070" and "2071-2100" bioclimatic data in CHELSA V.2.1.

--- clim.py
+++ clim.py
@@ -5,6 +5,10 @@ import glob
 from osgeo import gdal
 import sys
 
+# If sys.argv[3,4 and 5] are not defined, the script will run with the default values
+# If sys.argv[3,4 and 5] are defined, the script will run with the values defined by the user
+# The default values are: proj = "EPSG:4326", oubound = (-20, 20, 60, 60), res = 0.041666666666667
+
 def main(dirpath, contpathfile, proj = "EPSG:4326", oubound = (-20, 20, 60, 60), res = 0.041666666666667):  # WORK on implementing the arguments
     # Define all the files in the folder as a list named tifs
     pathini = sys.argv[1]
@@ -19,13 +23,28 @@ def main(dirpath, contpathfile, proj = "EPSG:4326", oubound = (-20, 20, 60, 60),
         # Define the output file name
         out = tif[:-4] + "_reprojected.tif"
         # Define the output projection
-        proj = proj
+        # The default projection is WGS84 (EPSG:4326)
+        # The projection can be changed by the user in the input arguments
+        if len(sys.argv) > 3:
+            proj = sys.argv[3]
+        else:
+            proj = "EPSG:4326"
         # Define boundaries of the output file
         # The boundaries are defined by the coordinates of the lower left and upper right corners
         # The coordinates are in the order of (minX, minY, maxX, maxY) default is (-20, 20, 60, 60)
-        oubound = oubound
+        # The boundaries can be changed by the user in the input arguments
+        if len(sys.argv) > 4:
+            # Take arguments from the command line and convert them to a tuple
+            oubound = tuple(map(float, sys.argv[4].split(",")))
+        else:
+            oubound = (-20, 20, 60, 60)
         # Define the output resolution for x and y
         # For this, output resolution is equal to 2.5 arc minutes as default
+        # The resolution can be changed by the user in the input arguments
+        if len(sys.argv) > 5:
+            res = float(sys.argv[5])
+        else:
+            res = 0.041666666666667
         resx = res
         resy = -1 * res
         # Reproject and resample the file
@@ -38,12 +57,37 @@ def main(dirpath, contpathfile, proj = "EPSG:4326", oubound = (-20, 20, 60, 60),
     outfiles = glob.glob(pathini + "*_reprojected_cropped.tif")
     print(outfiles)
     # Define the strings to be replaced
-    replace1 = "_1981-2010_V.2.1_reprojected_cropped"
-    replace2 = "_2011-2040_mri-esm2-0_ssp370_V.2.1_reprojected_cropped"
+    d01 = "_1981-2010"
+    d02 = "_2011-2040"
+    d03 = "_2041-2070"
+    d04 = "_2071-2100"
+    d05 = "_ssp126"
+    d06 = "_ssp370"
+    d07 = "_ssp585"
+    d08 = "_mri-esm2-0"
+    d09 = "_mpi-esm1-2-hr"
+    d10 = "_ukesm1-0-ll"
+    d11 = "_ipsl-cm6a-lr"
+    d12 = "_gfdl-esm4"
+    d13 = "_V.2.1_reprojected_cropped"
+    # For loop to rename the files
     for outfile in outfiles:
-        newname = outfile.replace("CHELSA_", "").replace(replace1, "").replace(replace2, "")
+        newname = outfile.replace("CHELSA_", "") \
+       .replace(d01, "") \
+       .replace(d02, "") \
+       .replace(d03, "") \
+       .replace(d04, "") \
+       .replace(d05, "") \
+       .replace(d06, "") \
+       .replace(d07, "") \
+       .replace(d08, "") \
+       .replace(d09, "") \
+       .replace(d10, "") \
+       .replace(d11, "") \
+       .replace(d12, "") \
+       .replace(d13, "")
         print(newname)
         gdal.Rename(outfile, newname)
 
 
-main(sys.argv[1], sys.argv[2])
+main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
